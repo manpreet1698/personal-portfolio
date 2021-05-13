ba144e80862ec3cd4fec279519b15445282bb64f
Manpreet Singh <manpreetsingh@Manpreets-MacBook-Air.local>
2021-05-13 15:23:49 -0400

    new resume and responsive at 360

--- main.js
+++ main.js
@@ -29,3 +29,14 @@ window.addEventListener("scroll", () =>{
     }
 })
 navSlider();
+
+function submitForm() {
+    // Get the first form with the name
+    // Usually the form name is not repeated
+    // but duplicate names are possible in HTML
+    // Therefore to work around the issue, enforce the correct index
+    var frm = document.getElementsByName('simple-contact-form')[0];
+    frm.submit(); // Submit the form
+    frm.reset();  // Reset all form data
+    return false; // Prevent page refresh
+ }
